2ca639282254a35e0e9431148f00fa1ff1e06e86
cesare.soldini <cesare.soldini@accurat.it>
2017-12-22 18:27:31 +0100

    ✨ Implement Hot loader in accurapp

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,23 @@
 const buildWebpackConfig = require('webpack-preset-accurapp')
-module.exports = buildWebpackConfig()
+const browsers = process.env.NODE_ENV === 'development' ? ['last 1 Chrome version'] : ['last 2 versions', 'ie 10']
+const babelrc = require('webpack-preset-accurapp/babelrc')(browsers)
+const { babel } = require('webpack-blocks')
+
+function reactHotLoader() {
+  return (context, { addLoader }) => config => {
+    config.entry.main.splice(1, 0, 'react-hot-loader/patch')
+    return config
+  }
+}
+
+const babelrcHot = {
+  ...babelrc,
+  plugins: babelrc.plugins.concat([
+    'react-hot-loader/babel',
+  ]),
+}
+
+module.exports = buildWebpackConfig([
+  reactHotLoader(),
+  babel(babelrcHot),
+])
